3b9521302eeddb6d11f67fb28c63750b1af55500
jnthnch <jchu127@gmail.com>
2019-03-13 20:17:59 -0700

    Implemented schema design for chat client

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,30 @@
+/* Create other tables and define schemas for them here! */
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  room_id INT,
+  message VARCHAR(140) NULL DEFAULT NULL
 );
 
-/* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY, 
+  name VARCHAR(20) NOT NULL
+);
 
+CREATE TABLE rooms (
+  id INT AUTO_INCREMENT PRIMARY KEY, 
+  name VARCHAR(20) NOT NULL
+);
 
+ALTER TABLE `messages` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (room_id) REFERENCES `rooms` (`id`);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
